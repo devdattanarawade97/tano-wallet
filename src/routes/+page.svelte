01b7972ca0b951c7f05456b96c19fb6f4155b69e
devdatta <devdattanarawade97@gmail.com>
2024-08-20 17:28:00 +0530

    first version

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+
+
+
+<main>
+    <h1 class="text-center my-5">Welcome to Tano Wallet</h1>
+    <h3 class="text-center">Wallet balance : 0.00$</h3>
+    
+    <div class="d-flex justify-content-center align-items-center " id="button-header">
+     
+        <button class="mx-5 my-5 rounded-3 bg-primary text-white p-2 px-5">Pay</button>
+        <button class="mx-5 my-5 rounded-3 bg-primary text-white p-2 px-5">add</button>
+    
+    
+    </div> 
+     <div class="d-flex text-center justify-content-center" id="footer">
+        <button class="mx-5 my-5 rounded-3 bg-primary text-white p-2 px-5">
+        Close Wallet
+     </button></div>
+</main>
