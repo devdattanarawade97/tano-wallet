b305739e635ae8fbd3c99ecb0f73f4b2b522afe3
devdatta <devdattanarawade97@gmail.com>
2024-08-22 21:48:36 +0530

    ton keeper added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -41,11 +41,12 @@
 						name: "Tonkeeper",
 						tondns: "tonkeeper.ton",
 						jsBridgeKey: "tonkeeper",
+					     
 						imageUrl: "https://tonkeeper.com/assets/tonconnect-icon.png",
 						aboutUrl: "https://tonkeeper.com",
 						universalLink: "https://app.tonkeeper.com/ton-connect",
-						 bridgeUrl: "https://bridge.tonapi.io/bridge",
-						//  bridgeUrl: "https://testnet.tonapi.io/bridge",
+						//  bridgeUrl: "https://bridge.tonapi.io/bridge",
+						  bridgeUrl: "https://testnet.tonapi.io/bridge",
 
 						platforms: [
 							"ios",
@@ -58,6 +59,7 @@
 							"linux",
 						],
 					},
+					
 				],
 			},
 		});
